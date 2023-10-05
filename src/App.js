ee59ed1e0c4bf487790ee631b3f0e55d49f23f28
Liubov5 <liubov.iakovleva5@gmail.com>
2023-10-05 22:30:13 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <>
       <div id="sidebar">
-          <h1>React Router Contacts</h1>
+          <h1>React Router Tutorial</h1>
           <div>
             <Form id="search-form" role="search">
               <input
